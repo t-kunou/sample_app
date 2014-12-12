e1841e4737b2e9d7435b8801842143a2cdd53c1e
kunou <t-kunou@esm.co.jp>
2014-12-12 10:10:46 +0900

    Finish 9

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -27,22 +27,39 @@ describe "Authentication" do
     end
 
     describe "with valid infomation" do
-      let(:user) { FactoryGirl.create(:user) }
-      before do
-        #valid_signin(user)
-        sign_in user
+        let(:user) { FactoryGirl.create(:user) }
+
+      describe "before sign in" do
+        it { should_not have_link('Profile', href: user_path(user)) }
+        it { should_not have_link('Settings', href: edit_user_path(user)) }
+        it { should_not have_link('Sign out', href: signout_path ) }
       end
 
-      it { should have_title(user.name) }
-      it { should have_link('Users', href: users_path) }
-      it { should have_link('Profile', href: user_path(user)) }
-      it { should have_link('Settings', href: edit_user_path(user)) }
-      it { should have_link('Sign out', href: signout_path ) }
-      it { should_not have_link('sign in', href: signin_path) }
+      describe "after sign in" do
+        before do
+          sign_in user
+        end
 
-      describe "followed by signout" do
-        before { click_link "Sign out" }
-        it { should have_link('Sign in') }
+        it { should have_title(user.name) }
+        it { should have_link('Users', href: users_path) }
+        it { should have_link('Profile', href: user_path(user)) }
+        it { should have_link('Settings', href: edit_user_path(user)) }
+        it { should have_link('Sign out', href: signout_path ) }
+        it { should_not have_link('sign in', href: signin_path) }
+
+        describe "followed by signout" do
+          before { click_link "Sign out" }
+          it { should have_link('Sign in') }
+        end
+
+        describe "after visitiong signup page" do
+          before do
+            click_link "Home"
+            click_link "Sign up"
+          end
+          it { should have_content('Welcome') }
+        end
+
       end
     end
   end
@@ -73,9 +90,7 @@ describe "Authentication" do
       describe "when attempting to visit a protected page" do
         before do
           visit edit_user_path(user)
-          fill_in "Email",    with: user.email
-          fill_in "Password", with: user.password
-          click_button "Sign in"
+          sign_in user
         end
 
         describe "after signing in" do
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -92,7 +92,7 @@ describe "UserPages" do
         fill_in "Name", with: "Example User"
         fill_in "Email", with: "user@example.com"
         fill_in "Password", with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in "Confirm Password", with: "foobar"
       end
 
       it "should create a suser" do
@@ -107,8 +107,17 @@ describe "UserPages" do
         it { should have_title(user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
-
+     
+      describe "after signed in" do
+        let(:user) { FactoryGirl.create(:user) }
+        before do
+          sign_in user, no_capybara: true
+          click_button submit
+        end
+        it { should have_content('Welcome') }
+      end
     end
+
 
     describe "edit" do
       let(:user) { FactoryGirl.create(:user) }
@@ -128,6 +137,7 @@ describe "UserPages" do
 
         it { should have_content('error') }
       end
+
       describe "with valid information" do
         let(:new_name)  { "New Name" }
         let(:new_email) { "new@example.com" }
@@ -144,6 +154,18 @@ describe "UserPages" do
         it { should have_link('Sign out', href: signout_path) }
         specify { expect(user.reload.name).to  eq new_name }
         specify { expect(user.reload.email).to eq new_email }
+      end
+    
+      describe "forbidden attributes" do
+        let(:params) do
+          { user: { admin: true, password: user.password,
+                    password_confirmation: user.password } }
+        end
+        before do
+          sign_in user, no_capybara: true
+          patch user_path(user), params
+        end
+        specify { expect(user.reload).not_to be_admin }
       end
     end
   end
